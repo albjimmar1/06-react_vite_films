449d13fa24bac221bc7ff4d2ee2618ab4d47ed9b
albjimmar1 <47810351+albjimmar1@users.noreply.github.com>
2023-04-26 13:11:58 +0200

    Void form

---
+++ src/App.jsx
@@ -0,0 +1,19 @@
+import './App.css'
+
+function App() {
+  return (
+    <div className='page'>
+      <header>
+        <form className='form'>
+          <input type='text' placeholder='Avengers, Dangeons & Dragons, Dune...'/>
+          <button type='submit'/>
+        </form>
+      </header>
+      <main>
+        Results
+      </main>
+    </div>
+  );
+}
+
+export default App
