63d117294711c2b5af6d0c60cef0fd3a120c411b
albjimmar1 <47810351+albjimmar1@users.noreply.github.com>
2023-05-09 12:21:58 +0200

    Configuration settings

--- src/services/movies.js
+++ src/services/movies.js
@@ -1,19 +1,19 @@
-const API_KEY = '4287ad07';
+import { API_ENDPOINT, API_KEY } from "../config.js"
 
 export const searchMovies = async ({ search }) => {
     if (search === '') return null;
 
     try {
-        const response = await fetch(`https://www.omdbapi.com/?apikey=${API_KEY}&s=${search}`);
-        const json = await response.json();
-        const movies = json.Search;
+        const response = await fetch(`${API_ENDPOINT}?apikey=${API_KEY}&s=${search}`)
+        const json = await response.json()
+        const movies = json.Search
         return movies?.map(movie => ({
             id: movie.imdbID,
             title: movie.Title,
             year: movie.Year,
             poster: movie.Poster
-        }));
+        }))
     } catch (e) {
-        throw new Error('Error searching movies');
+        throw new Error('Error searching movies')
     }
 }
